cafcc50e5e45722a3e28720515fe6dc3a8274dcd
raghav581 <raghav.khandelwal1004@gmail.com>
2025-05-08 17:26:53 +0530

    Added Admin pages

---
+++ src/pages/admin/IndexPage.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1>Lunding</h1>
+
+</template>
--- src/router/routes.js
+++ src/router/routes.js
@@ -6,7 +6,14 @@ const routes = [
       { path: '', component: () => import('pages/IndexPage.vue') }
     ]
   },
-
+  {
+    path: '/admin',
+    component: () => import('layouts/AdminLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/admin/IndexPage.vue') },
+      { path: 'login', component: () => import('pages/admin/LoginPage.vue') }
+    ]
+  },
   // Always leave this as last one,
   // but you can also remove it
   {
